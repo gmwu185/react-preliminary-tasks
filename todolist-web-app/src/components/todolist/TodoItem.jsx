35375d8a7c6cb48883d8c17524ab273665ce2518
JimmyWu <gmwu185@gmail.com>
2022-09-03 00:41:22 +0800

    新增 移除代辦功能 (單筆)

--- todolist-web-app/src/components/todolist/TodoItem.jsx
+++ todolist-web-app/src/components/todolist/TodoItem.jsx
@@ -1,11 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { useDatasContext } from '../../controllers/contexts';
 
-const hrefLink = '#';
+import { useDatasContext, useAuth } from '../../controllers/contexts';
+import { api_deleteItem, api_todoList } from '../../controllers/todos';
 
 const TodoItem = () => {
-  const { todosData, selectType, setSelectType } = useDatasContext();
+  const { todosData, setTodosData } = useDatasContext();
+  const { token } = useAuth();
+  const hrefLink = '#';
+
+  const delTodo = ({ e, todo }) => {
+    const { id, content } = todo;
+    const asyncDelTodo = async () => {
+      const delTodoRes = await api_deleteItem(token, id);
+      const delTodoResJson = await delTodoRes.json();
+      if (delTodoRes.status === 200) {
+        const todoListRes = await api_todoList(token);
+        const todoListResJson = await todoListRes.json();
+        setTodosData(todoListResJson.todos);
+        alert(`${delTodoResJson.message} ${content}`);
+      } else {
+        alert(delTodoResJson.message);
+      }
+    };
+    asyncDelTodo();
+    e.preventDefault();
+  };
 
   return todosData.map((todo, i) => {
     return (
@@ -14,7 +34,7 @@ const TodoItem = () => {
           <input className="todoList_input" type="checkbox" value="true" />
           <span>{todo.content}</span>
         </label>
-        <a href={hrefLink}>
+        <a href={hrefLink} onClick={(e) => delTodo({ e, todo })}>
           <FontAwesomeIcon icon={faTimes} />
         </a>
       </li>
