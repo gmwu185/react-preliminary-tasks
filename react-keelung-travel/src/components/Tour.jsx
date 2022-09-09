2514df33e61085e21ae7046e79afb0a3bc286e99
JimmyWu <gmwu185@gmail.com>
2022-09-09 10:00:17 +0800

    增加 cors proxy service 取跨域資料方式 (備案)

--- react-keelung-travel/src/components/Tour.jsx
+++ react-keelung-travel/src/components/Tour.jsx
@@ -10,6 +10,9 @@ function Tour() {
   useEffect(() => {
     SwalLoader();
     if (tourDatas.length === 0) {
+      /** cors proxy service use-1
+        * 可直接使用，但會限流量與次數
+      */
       fetch(
         'https://cors.eu.org/https://tour.klcg.gov.tw/data/attractions.json/'
       )
@@ -28,7 +31,53 @@ function Tour() {
           console.log(err);
           return msgSwal(`發生錯誤：${err.message}`);
         });
+      /* /cors proxy service use-1 */
+
+      /** cors proxy service use-2
+        * 需進入畫面點按執行按鈕才可使用
+      */
+      // fetch("https://cors-anywhere.herokuapp.com/https://tour.klcg.gov.tw/data/attractions.json")
+      //   .then((res) => res.json())
+      //   .then( (result) => {
+      //     setRourDatas(result.attractions);
+      //     Swal.fire({
+      //       position: 'top-end',
+      //       icon: 'info',
+      //       html: `<small>完整旅遊景點資料已完全載入頁面，可透過選擇地區過濾操作！</small>`,
+      //       // showConfirmButton: false,
+      //       timer: 3000,
+      //     });
+      //   })
+      //   .catch(err => {
+      //     console.log(err);
+      //     return msgSwal(`發生錯誤：${err.message}`);
+      //   })
+      /* /cors proxy service use-2 */
+      
+      /** cors proxy service use-3
+        * https://github.com/Freeboard/thingproxy
+      */
+      // fetch("https://thingproxy.freeboard.io/fetch/https://tour.klcg.gov.tw/data/attractions.json")
+      //   .then((res) => res.json())
+      //   .then( (data) => {
+      //     console.log('thingproxy data', data)
+      //   })
+      //   .catch(err => console.log('thingproxy err', err))
+      /* /cors proxy service use-3 */
+      
+      /** cors proxy service use-4
+       * https://allorigins.win/
+       */
+      // fetch(`https://api.allorigins.win/get?url=${'https://tour.klcg.gov.tw/data/attractions.json'}`)
+      //   .then((res) => res.json())
+      //   .then( (data) => {
+      //     console.log('allorigins data.contents', parseJSON(data.contents))
+      //   })
+      //   .catch(err => console.log('allorigins err', err))
+      /* /cors proxy service use-4 */
     }
+
+    
     // eslint-disable-next-line
   }, []);
 
