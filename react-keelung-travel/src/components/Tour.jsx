e658b5c13cdb162ba73cf852c268d3971e428224
JimmyWu <gmwu185@gmail.com>
2023-10-28 15:18:12 +0800

    1. cors proxy service 取跨域資料方式 (調整使用備案方式)   2. 程式碼格式化   3. result 函式以變數共用

--- react-keelung-travel/src/components/Tour.jsx
+++ react-keelung-travel/src/components/Tour.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import Swal from "sweetalert2";
 
-import { SwalLoader, msgSwal } from '../vendorModule/Swal';
+import { SwalLoader, msgSwal } from "../vendorModule/Swal";
 
 function Tour() {
   const [tourDatas, setRourDatas] = useState([]);
@@ -10,32 +10,33 @@ function Tour() {
   useEffect(() => {
     SwalLoader();
     if (tourDatas.length === 0) {
-      /** cors proxy service use-1
-        * 可直接使用，但會限流量與次數
-      */
-      fetch(
-        'https://cors.eu.org/https://tour.klcg.gov.tw/data/attractions.json/'
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          setRourDatas(result.attractions);
-          Swal.fire({
-            position: 'top-end',
-            icon: 'info',
-            html: `<small>完整旅遊景點資料已完全載入頁面，可透過選擇地區過濾操作！</small>`,
-            // showConfirmButton: false,
-            timer: 3000,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          return msgSwal(`發生錯誤：${err.message}`);
+      const proxySuccessResult = (result) => {
+        setRourDatas(result.attractions);
+        Swal.fire({
+          position: "top-end",
+          icon: "info",
+          html: `<small>完整旅遊景點資料已完全載入頁面，可透過選擇地區過濾操作！</small>`,
+          timer: 3000,
         });
+      };
+
+      /** cors proxy service use-1
+       * 可直接使用，但會限流量與次數
+       */
+      // fetch(
+      //   'https://cors.eu.org/https://tour.klcg.gov.tw/data/attractions.json/'
+      // )
+      //   .then((res) => res.json())
+      //   .then( proxySuccessResult )
+      //   .catch((err) => {
+      //     console.log(err);
+      //     return msgSwal(`發生錯誤：${err.message}`);
+      //   });
       /* /cors proxy service use-1 */
 
       /** cors proxy service use-2
-        * 需進入畫面點按執行按鈕才可使用
-      */
+       * 需進入畫面點按執行按鈕才可使用
+       */
       // fetch("https://cors-anywhere.herokuapp.com/https://tour.klcg.gov.tw/data/attractions.json")
       //   .then((res) => res.json())
       //   .then( (result) => {
@@ -53,18 +54,21 @@ function Tour() {
       //     return msgSwal(`發生錯誤：${err.message}`);
       //   })
       /* /cors proxy service use-2 */
-      
+
       /** cors proxy service use-3
-        * https://github.com/Freeboard/thingproxy
-      */
-      // fetch("https://thingproxy.freeboard.io/fetch/https://tour.klcg.gov.tw/data/attractions.json")
-      //   .then((res) => res.json())
-      //   .then( (data) => {
-      //     console.log('thingproxy data', data)
-      //   })
-      //   .catch(err => console.log('thingproxy err', err))
+       * https://github.com/Freeboard/thingproxy
+       */
+      fetch(
+        "https://thingproxy.freeboard.io/fetch/https://tour.klcg.gov.tw/data/attractions.json"
+      )
+        .then((res) => res.json())
+        .then(proxySuccessResult)
+        .catch((err) => {
+          console.log(err);
+          return msgSwal(`發生錯誤：${err.message}`);
+        });
       /* /cors proxy service use-3 */
-      
+
       /** cors proxy service use-4
        * https://allorigins.win/
        */
@@ -77,7 +81,6 @@ function Tour() {
       /* /cors proxy service use-4 */
     }
 
-    
     // eslint-disable-next-line
   }, []);
 
